d63aebb5b4bbb0952c7b2fea033ffaa4e24063f7
Vaibhao Falode <vaibhaofalode@gmail.com>
2024-02-08 13:43:15 +0530

    Added input logic for moves

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 class Chandrayaan {
 
   constructor() {
-    this.inputs = '';
+    this.inputs = [];
     this.direction = 'N';
     this.position = { x: 0, y: 0, z: 0 }; // [0, 0, 0]
   }
@@ -19,9 +19,23 @@ class Chandrayaan {
     return this.inputs;
   }
 
+  addInput(char) {
+    if (!['f', 'b', 'l', 'r', 'u', 'd'].includes(char)) {
+      console.log('Invalid Character');
+    } else {
+      this.inputs.push(char);
+    }
+  }
+  
 }
 
 a = new Chandrayaan();
+
+a.addInput('f')
+a.addInput('r')
+a.addInput('u')
+a.addInput('b')
+a.addInput('l')
 
 console.log('getInputs', a.getInputs())
 console.log('getDirection => ', a.getDirection())
